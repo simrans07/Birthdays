96b3975084328ed167c3fb8bcb7d0af766d44cde
simran s <sps022011@gmail.com>
2025-07-25 10:43:35 -0700

    add birthdays feature, fixed add birthdays buttons

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var friends : [Friend] = [Friend(name: "Aditi", birthday: .now), Friend(name: "Tracey", birthday:Date(timeIntervalSince1970: 20)) ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack{
@@ -20,8 +22,26 @@ struct ContentView: View {
                 }//hstack
             }//list
             .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment:.center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }//date picker
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }//button
+                    .bold()
+                }//vstack
+                .padding()
+                .background(.bar)
+            }//safe inset
         }//navstack
-        
     }//body
 }//struct
 
